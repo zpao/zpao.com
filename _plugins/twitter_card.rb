337b5c52b00a5329af1be232f03e3c45b9a3770e
Paul O’Shannessy <paul@oshannessy.com>
2013-04-21 12:41:46 -0700

    Only show twitter cards for posts
    Fixes the bug I have now where liquid templating is showing up on the
    page on the archive.

---
+++ _plugins/twitter_card.rb
@@ -0,0 +1,20 @@
+# A way to specify a Twitter card for posts that can still end up in my default
+# layout. This works around a Jekyll "bug" where page.layout is the parent
+# layout instead of the specific one.
+# Impl based on http://gistflow.com/posts/609-content_for-sidebar-in-jekyll
+module Jekyll
+  class TwitterCard < Liquid::Block
+    alias :super_render :render
+
+    def initialize(tag_name, identifier, tokens)
+      super
+    end
+
+    def render(context)
+      context.environments.first["page"]["twitter_card"] = super_render(context)
+      ''
+    end
+  end
+end
+
+Liquid::Template.register_tag('twitter_card', Jekyll::TwitterCard)
